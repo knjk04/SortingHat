84a0bae459a9ae3edf3798396c314efe811bb3e2
knjk04 <karan.n.j.kumar@gmail.com>
2018-07-07 16:27:31 +0100

    Reading 1st qs ans from file

--- app/src/main/java/com/presentedbykaran/sortinghat/Question.java
+++ app/src/main/java/com/presentedbykaran/sortinghat/Question.java
@@ -1,6 +1,7 @@
 package com.presentedbykaran.sortinghat;
 
 import android.content.Context;
+import android.util.Log;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -13,9 +14,10 @@ import java.util.ArrayList;
  */
 public class Question {
 
-    private static final String TAG = "QuestionsAndAnswers";
+    private static final String TAG = "Question";
 
-    private ArrayList<String> questions = new ArrayList<>();
+    private ArrayList<String> mQuestions = new ArrayList<>();
+    private ArrayList<String> mAnswers = new ArrayList<>();
 //    private String question;
     private String option1;
     private String option2;
@@ -27,16 +29,17 @@ public class Question {
     private House op3House;
     private House op4House;
 
-    private Context context;
+    private Context mContext;
 
-//    public Question(String question, String option1, String option2, String option3, String option4, House op1House, House op2House, House op3House, House op4House) {
-//        this.question = question;
-    public Question(Context context, String option1, String option2, String option3, String option4, House op1House, House op2House, House op3House, House op4House) {
-        this.context = context;
-        this.option1 = option1;
-        this.option2 = option2;
-        this.option3 = option3;
-        this.option4 = option4;
+    private final int NUM_OPTIONS = 4;
+
+//    public Question(Context context, String option1, String option2, String option3, String option4, House op1House, House op2House, House op3House, House op4House) {
+    public Question(Context context, House op1House, House op2House, House op3House, House op4House) {
+        mContext = context;
+//        this.option1 = option1;
+//        this.option2 = option2;
+//        this.option3 = option3;
+//        this.option4 = option4;
         this.op1House = op1House;
         this.op2House = op2House;
         this.op3House = op3House;
@@ -46,91 +49,59 @@ public class Question {
     }
 
     private void initQuestions() {
-        String data = "";
-//        ArrayList<String> questionsList = new ArrayList<>();
+        String data;
+        // starting from 0 and going up to 4 where 0 represents the question and 1-4 the respective option numbers
+        int currentOptionNum = 0;
 
-        InputStream inputStream = context.getResources().openRawResource(R.raw.questions);
+        InputStream inputStream = mContext.getResources().openRawResource(R.raw.questions);
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
 
         if(inputStream != null) {
             try {
                 while ((data = bufferedReader.readLine()) != null) {
-//                    questionsList.add(data);
-                    questions.add(data);
+                    Log.d(TAG, "In while loop");
+                    if(currentOptionNum == 0) mQuestions.add(data);
+                    else mAnswers.add(data);
+                    Log.d(TAG, "After adding question/answer");
+
+                    currentOptionNum++;
+                    Log.d(TAG, "After incrementing currentOptionNum");
+
+                    if(currentOptionNum > NUM_OPTIONS) currentOptionNum = 0; //reset
+
+                    Log.d(TAG, "After possibly resetting currentOptionNum");
+                    Log.d(TAG, "currentOptionNum = " + currentOptionNum);
                 }
                 inputStream.close();
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
+        Log.d(TAG, "End of initQuestions()");
     }
 
     public String getQuestion(int index) {
-        return questions.get(index);
+        return mQuestions.get(index);
 //        return question;
     }
 
-    public String getOption1() {
-        return option1;
-    }
-
-    public void setOption1(String option1) {
-        this.option1 = option1;
-    }
-
-    public String getOption2() {
-        return option2;
-    }
-
-    public void setOption2(String option2) {
-        this.option2 = option2;
-    }
-
-    public String getOption3() {
-        return option3;
-    }
-
-    public void setOption3(String option3) {
-        this.option3 = option3;
-    }
-
-    public String getOption4() {
-        return option4;
-    }
-
-    public void setOption4(String option4) {
-        this.option4 = option4;
+    public String getOption(int optionNum) {
+       return mAnswers.get(optionNum);
     }
 
     public House getOp1House() {
         return op1House;
     }
 
-    public void setOp1House(House op1House) {
-        this.op1House = op1House;
-    }
-
     public House getOp2House() {
         return op2House;
-    }
-
-    public void setOp2House(House op2House) {
-        this.op2House = op2House;
     }
 
     public House getOp3House() {
         return op3House;
     }
 
-    public void setOp3House(House op3House) {
-        this.op3House = op3House;
-    }
-
     public House getOp4House() {
         return op4House;
-    }
-
-    public void setOp4House(House op4House) {
-        this.op4House = op4House;
     }
 }
--- app/src/main/java/com/presentedbykaran/sortinghat/QuizActivity.java
+++ app/src/main/java/com/presentedbykaran/sortinghat/QuizActivity.java
@@ -35,7 +35,8 @@ public class QuizActivity extends AppCompatActivity implements View.OnClickListe
     private int mSlytherinTally  = 0;
 
 
-    private int currentIndexVal  = 0; //default value
+    private int currentIndexVal  = 0; // default value
+    private int questionNum = 0; // since answers ArrayList starts from 0
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -62,6 +63,7 @@ public class QuizActivity extends AppCompatActivity implements View.OnClickListe
 //            rdBtn4.setText(arrQAndA[i].getOption4());
 
 //            currentIndexVal = 0;
+        Log.d(TAG, "Before invoking nextQuestion()");
             nextQuestion();
 
 //            while (currentIndexVal < NUM_QUESTIONS_TO_ASK) {
@@ -125,148 +127,142 @@ public class QuizActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     public void nextQuestion() {
+//        for(int i = 1; currentIndexVal <= NUM_QUESTIONS_TO_ASK; i++) {
+//            for(int i = 1; currentIndexVal <= 1; i++) {
+//        int i = 1;
         if(currentIndexVal <= NUM_QUESTIONS_TO_ASK) {
+            Log.d(TAG, "currentIndexVal in nextQuestion(): " + currentIndexVal);
             mTxtQuestion.setText(arrQAndA[currentIndexVal].getQuestion(currentIndexVal));
-            rdBtn1.setText(arrQAndA[currentIndexVal].getOption1());
-            rdBtn2.setText(arrQAndA[currentIndexVal].getOption2());
-            rdBtn3.setText(arrQAndA[currentIndexVal].getOption3());
-            rdBtn4.setText(arrQAndA[currentIndexVal].getOption4());
+            rdBtn1.setText(arrQAndA[currentIndexVal].getOption(questionNum++));
+            rdBtn2.setText(arrQAndA[currentIndexVal].getOption(questionNum++));
+            rdBtn3.setText(arrQAndA[currentIndexVal].getOption(questionNum++));
+            rdBtn4.setText(arrQAndA[currentIndexVal].getOption(questionNum++));
+//            rdBtn1.setText(arrQAndA[currentIndexVal].getOption1());
+//            rdBtn2.setText(arrQAndA[currentIndexVal].getOption2());
+//            rdBtn3.setText(arrQAndA[currentIndexVal].getOption3());
+//            rdBtn4.setText(arrQAndA[currentIndexVal].getOption4());
         }
     }
 
     private void initQAndAArray() {
         int i = 0;
 
-//        Log.d(TAG, "val of i before first q: " + i);
-
-
-        arrQAndA[i++] = new Question(this,
-                "The small tortoiseshell box, embellished with gold, inside " +
-                        "which some small creature seems to be squeaking.",
-                "The gleaming jet black box with a silver lock and key, " +
-                        "marked with a mysterious rune that you know to be the mark " +
-                        "of Merlin.",
-                "The ornate golden casket, standing on clawed feet, whose " +
-                        "inscription warns that both secret knowledge and unbearable " +
-                        "temptation lie within.",
-                "The small pewter box, unassuming and plain, with a " +
-                        "scratched message upon it that reads ‘I open only for the " +
-                        "worthy.’", House.Hufflepuff, House.Slytherin, House.Ravenclaw,
-                House.Gryffindor);
+        arrQAndA[i++] = new Question(this, House.Hufflepuff, House.Slytherin,
+                House.Ravenclaw, House.Gryffindor);
 //        mTxtQuestion.setText(arrQAndA[i].getQuestion(i++));
 
         Log.d(TAG, "val of i after 1st q: " + i);
-
-        arrQAndA[i++] = new Question(this,
-                "Attempt to confuse the troll into letting all three of you pass without " +
-                        "fighting?",
-                "Suggest drawing lots to decide which of you will fight?",
-                "Suggest that all three of you should fight (without telling the troll)?",
-                "Volunteer to fight?", House.Ravenclaw, House.Hufflepuff, House.Slytherin,
-                House.Gryffindor);
-
-        Log.d(TAG, "val of i after 2nd q: " + i);
-
-       arrQAndA[i++] = new Question(this,
-                    "A crackling log fire",
-                    "The sea",
-                    "Fresh parchment",
-                    "Home", House.Gryffindor, House.Slytherin, House.Ravenclaw,
-               House.Hufflepuff);
-
-        Log.d(TAG, "val of i after 3rd q: " + i);
-
-       arrQAndA[i++] = new Question(this,
-                "Lie and say you don’t know (but hope that somebody else tells Professor " +
-                        "Flitwick the truth).",
-                "Tell Professor Flitwick that he ought to ask your classmate (and resolve to " +
-                        "tell your classmate that if he doesn’t tell the truth, you will).",
-                "Tell Professor Flitwick the truth. If your classmate is prepared to win by " +
-                        "cheating, he deserves to be found out. Also, as you are both in the same " +
-                        "house, any points he loses will be regained by you, for coming first in his " +
-                        "place.",
-                "You would not wait to be asked to tell Professor Flitwick the truth. If you " +
-                        "knew that somebody was using a forbidden quill, you would tell the teacher " +
-                        "before the exam started.",House.Hufflepuff,House.Gryffindor,House.Ravenclaw,
-                House.Slytherin);
-
-       arrQAndA[i++] = new Question(this,
-                "The silver leafed tree bearing golden apples",
-                "The fat red toadstools that appear to be talking to each other",
-                "The bubbling pool, in the depths of which something luminous is swirling",
-                "The statue of an old wizard with a strangely twinkling eye",
-                 House.Ravenclaw, House.Hufflepuff, House.Slytherin, House.Gryffindor);
-
-       arrQAndA[i++] = new Question(this,
-                "The foaming, frothing, silvery liquid that sparkles as though containing " +
-                        "ground diamonds.",
-                "The smooth, thick, richly purple drink that gives off a delicious smell of " +
-                        "chocolate and plums.",
-                "The golden liquid so bright that it hurts the eye, and which makes sunspots " +
-                        "dance all around the room.",
-                "The mysterious black liquid that gleams like ink, and gives off fumes that " +
-                        "make you see strange visions.",
-                 House.Ravenclaw, House.Hufflepuff, House.Gryffindor, House.Slytherin);
-
-       arrQAndA[i++] = new Question(this,
-                                    "The violin", "The trumpet", "The piano",
-                                    "The drum", House.Slytherin, House.Hufflepuff,
-                                     House.Ravenclaw, House.Gryffindor);
-
-       arrQAndA[i++] = new Question(this,
-               "Ordinary", "Ignorant", "Cowardly",
-               "Selfish", House.Slytherin, House.Ravenclaw, House.Gryffindor,
-                House.Hufflepuff);
-
-       arrQAndA[i++] = new Question(this,
-                "Miss you, but smile","Ask for more stories about your adventures",
-                "Think with admiration of your achievements",
-                "I don't care what people think of me after I'm dead; it's what they think of " +
-                       "me while I'm alive that counts", House.Hufflepuff, House.Gryffindor,
-                 House.Ravenclaw, House.Slytherin);
-
-       arrQAndA[i++] = new Question(this,
-                "The Wise", "The Good", "The Great", "The Bold",
-                 House.Ravenclaw, House.Hufflepuff, House.Slytherin, House.Gryffindor);
-
-       arrQAndA[i++] = new Question(this,
-                "Ask what makes them think so?",
-                "Agree, and ask whether they’d like a free sample of a jinx?",
-                "Agree, and walk away, leaving them to wonder whether you are bluffing?",
-                "Tell them that you are worried about their mental health, and offer to call " +
-                        "a doctor.", House.Ravenclaw, House.Slytherin, House.Gryffindor,
-                 House.Hufflepuff);
-
-       arrQAndA[i++] = new Question(this,
-                "Standing on top of something very high and realizing suddenly that there " +
-                        "are no hand- or footholds, nor any barrier to stop you falling.",
-                "An eye at the keyhole of the dark, windowless room in which you are locked.",
-                "Waking up to find that neither your friends nor your family have any idea " +
-                        "who you are.",
-                "Being forced to speak in such a silly voice that hardly anyone can " +
-                        "understand you, and everyone laughs at you.",
-                 House.Ravenclaw, House.Gryffindor, House.Hufflepuff, House.Slytherin);
 //
-       arrQAndA[i++] = new Question(this,
-                "Love?", "Glory?", "Wisdom?", "Power?",
-                House.Hufflepuff, House.Gryffindor, House.Ravenclaw, House.Slytherin);
+//        arrQAndA[i++] = new Question(this,
+//                "Attempt to confuse the troll into letting all three of you pass without " +
+//                        "fighting?",
+//                "Suggest drawing lots to decide which of you will fight?",
+//                "Suggest that all three of you should fight (without telling the troll)?",
+//                "Volunteer to fight?", House.Ravenclaw, House.Hufflepuff, House.Slytherin,
+//                House.Gryffindor);
+//
+//        Log.d(TAG, "val of i after 2nd q: " + i);
+//
+//       arrQAndA[i++] = new Question(this,
+//                    "A crackling log fire",
+//                    "The sea",
+//                    "Fresh parchment",
+//                    "Home", House.Gryffindor, House.Slytherin, House.Ravenclaw,
+//               House.Hufflepuff);
+//
+//        Log.d(TAG, "val of i after 3rd q: " + i);
+//
+//       arrQAndA[i++] = new Question(this,
+//                "Lie and say you don’t know (but hope that somebody else tells Professor " +
+//                        "Flitwick the truth).",
+//                "Tell Professor Flitwick that he ought to ask your classmate (and resolve to " +
+//                        "tell your classmate that if he doesn’t tell the truth, you will).",
+//                "Tell Professor Flitwick the truth. If your classmate is prepared to win by " +
+//                        "cheating, he deserves to be found out. Also, as you are both in the same " +
+//                        "house, any points he loses will be regained by you, for coming first in his " +
+//                        "place.",
+//                "You would not wait to be asked to tell Professor Flitwick the truth. If you " +
+//                        "knew that somebody was using a forbidden quill, you would tell the teacher " +
+//                        "before the exam started.",House.Hufflepuff,House.Gryffindor,House.Ravenclaw,
+//                House.Slytherin);
+//
+//       arrQAndA[i++] = new Question(this,
+//                "The silver leafed tree bearing golden apples",
+//                "The fat red toadstools that appear to be talking to each other",
+//                "The bubbling pool, in the depths of which something luminous is swirling",
+//                "The statue of an old wizard with a strangely twinkling eye",
+//                 House.Ravenclaw, House.Hufflepuff, House.Slytherin, House.Gryffindor);
+//
+//       arrQAndA[i++] = new Question(this,
+//                "The foaming, frothing, silvery liquid that sparkles as though containing " +
+//                        "ground diamonds.",
+//                "The smooth, thick, richly purple drink that gives off a delicious smell of " +
+//                        "chocolate and plums.",
+//                "The golden liquid so bright that it hurts the eye, and which makes sunspots " +
+//                        "dance all around the room.",
+//                "The mysterious black liquid that gleams like ink, and gives off fumes that " +
+//                        "make you see strange visions.",
+//                 House.Ravenclaw, House.Hufflepuff, House.Gryffindor, House.Slytherin);
+//
+//       arrQAndA[i++] = new Question(this,
+//                                    "The violin", "The trumpet", "The piano",
+//                                    "The drum", House.Slytherin, House.Hufflepuff,
+//                                     House.Ravenclaw, House.Gryffindor);
+//
+//       arrQAndA[i++] = new Question(this,
+//               "Ordinary", "Ignorant", "Cowardly",
+//               "Selfish", House.Slytherin, House.Ravenclaw, House.Gryffindor,
+//                House.Hufflepuff);
+//
+//       arrQAndA[i++] = new Question(this,
+//                "Miss you, but smile","Ask for more stories about your adventures",
+//                "Think with admiration of your achievements",
+//                "I don't care what people think of me after I'm dead; it's what they think of " +
+//                       "me while I'm alive that counts", House.Hufflepuff, House.Gryffindor,
+//                 House.Ravenclaw, House.Slytherin);
+//
+//       arrQAndA[i++] = new Question(this,
+//                "The Wise", "The Good", "The Great", "The Bold",
+//                 House.Ravenclaw, House.Hufflepuff, House.Slytherin, House.Gryffindor);
+//
+//       arrQAndA[i++] = new Question(this,
+//                "Ask what makes them think so?",
+//                "Agree, and ask whether they’d like a free sample of a jinx?",
+//                "Agree, and walk away, leaving them to wonder whether you are bluffing?",
+//                "Tell them that you are worried about their mental health, and offer to call " +
+//                        "a doctor.", House.Ravenclaw, House.Slytherin, House.Gryffindor,
+//                 House.Hufflepuff);
+//
+//       arrQAndA[i++] = new Question(this,
+//                "Standing on top of something very high and realizing suddenly that there " +
+//                        "are no hand- or footholds, nor any barrier to stop you falling.",
+//                "An eye at the keyhole of the dark, windowless room in which you are locked.",
+//                "Waking up to find that neither your friends nor your family have any idea " +
+//                        "who you are.",
+//                "Being forced to speak in such a silly voice that hardly anyone can " +
+//                        "understand you, and everyone laughs at you.",
+//                 House.Ravenclaw, House.Gryffindor, House.Hufflepuff, House.Slytherin);
 
-       arrQAndA[i++] = new Question(this,
-                "The wide, sunny, grassy lane", "The narrow, dark, lantern-lit " +
-               "alley", "The twisting, leaf-strewn path through woods", "The cobbled " +
-               "street lined with ancient buildings", House.Hufflepuff, House.Slytherin,
-                House.Gryffindor, House.Ravenclaw);
-
-//       Log.d(TAG, "val of i before final q: " + i);
-
-       arrQAndA[i++] = new Question(this,
-               "Proceed with caution, keeping one hand on your concealed wand and an eye out " +
-                       "for any disturbance?",
-                "Draw your wand and try to discover the source of the noise?",
-                "Draw your wand and stand your ground?",
-                "Withdraw into the shadows to await developments, while mentally reviewing " +
-                        "the most appropriate defensive and offensive spells, should trouble occur?",
-               House.Hufflepuff, House.Gryffindor, House.Slytherin, House.Ravenclaw);
+//       arrQAndA[i++] = new Question(this,
+//                "Love?", "Glory?", "Wisdom?", "Power?",
+//                House.Hufflepuff, House.Gryffindor, House.Ravenclaw, House.Slytherin);
+//
+//       arrQAndA[i++] = new Question(this,
+//                "The wide, sunny, grassy lane", "The narrow, dark, lantern-lit " +
+//               "alley", "The twisting, leaf-strewn path through woods", "The cobbled " +
+//               "street lined with ancient buildings", House.Hufflepuff, House.Slytherin,
+//                House.Gryffindor, House.Ravenclaw);
+//
+       Log.d(TAG, "val of i before final q: " + i);
+//
+//       arrQAndA[i++] = new Question(this,
+//               "Proceed with caution, keeping one hand on your concealed wand and an eye out " +
+//                       "for any disturbance?",
+//                "Draw your wand and try to discover the source of the noise?",
+//                "Draw your wand and stand your ground?",
+//                "Withdraw into the shadows to await developments, while mentally reviewing " +
+//                        "the most appropriate defensive and offensive spells, should trouble occur?",
+//               House.Hufflepuff, House.Gryffindor, House.Slytherin, House.Ravenclaw);
     }
 
     private String determineHouse() {
